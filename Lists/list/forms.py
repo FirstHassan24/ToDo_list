4d033098e75a85e179d006991992ebcd8dcc67cb
FirstHassan24 <akilou.issifou23@gmail.com>
2026-01-15 15:04:09 -0500

    Add form handling for list creation and create forms.py file

---
+++ Lists/list/forms.py
@@ -0,0 +1,3 @@
+#import forms so django knows what your trying to do:
+from django import forms
+from .models import List,Item
--- Lists/list/views.py
+++ Lists/list/views.py
@@ -7,4 +7,7 @@ from django.contrib import messages
 # Create your views here.
 #1. make a view where a user can create a list:
 def create_list(request):
-    
+    if request.method == "POST":
+        form =
+    #TODO:create a form.py file so the user can type the list info
+
