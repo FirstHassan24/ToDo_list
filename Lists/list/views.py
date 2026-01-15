35c72734a2e7d9ab1f284c261fc4bc9276e6c65e
FirstHassan24 <akilou.issifou23@gmail.com>
2026-01-15 09:50:44 -0500

    end: iv come to realize an order for me to create a a CRUD function im going to need a form for the C portion

--- Lists/list/views.py
+++ Lists/list/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_404
+#import the models:
+from .models import List,Item
+#import messages for error handling:
+from django.contrib import messages
 
 # Create your views here.
+#1. make a view where a user can create a list:
+def create_list(request):
+    
