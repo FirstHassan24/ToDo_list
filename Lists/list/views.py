55d0b5c2888eda7967b55e08b7d195311f845006
FirstHassan24 <akilou.issifou23@gmail.com>
2026-01-21 17:05:08 -0500

    Add create_list function to handle list creation form submission

--- Lists/list/views.py
+++ Lists/list/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render,redirect
 #import the models:
 from .models import List,Item
+#import the forms:
+from .forms import ListForm
 #import messages for error handling:
 from django.contrib import messages
-#2.make a function so the user can can create a category:
+#2.make a function so the user can can create a category,link it to the listform:
+def create_list(request):
+    #check if the user clicked the link:
+    if request.method =="POST":
+        #fill the form with data the user inputs:
+        form=ListForm(request.POST)
+        #chech if the form fields are valid(non empy,correctly typed,etc):
+        if form.is_valid():
+                
 
 
